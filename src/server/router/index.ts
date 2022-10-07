3b3a8eb86f26d66b05eb265ea10ceac4b565c45e
KoyoiDev <koyoi.dev@gmail.com>
2022-10-07 15:15:34 +0000

    remove example router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,14 +2,12 @@
 import { createRouter } from './context';
 import superjson from 'superjson';
 
-import { exampleRouter } from './example';
 import { nhentaiRouter } from './nhentai';
 import { javActressRouter } from './jav-actress';
 import { pururinRouter } from './pururin';
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge('example.', exampleRouter)
   .merge('nhentai.', nhentaiRouter)
   .merge('jav-actress.', javActressRouter)
   .merge('pururin.', pururinRouter);
